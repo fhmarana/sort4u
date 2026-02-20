04a1e0d9645bf93068e4361e5f0f80af37aa50f7
Kirby Acoba <godwinkirby@gmail.com>
2026-02-20 11:54:32 +0800

    re-construct the folder structure

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,3 @@
 from .user import User
 from .memory import Memory
+from .profile import Profile
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,11 @@ import './App.css'
 import { Route, Routes, Link } from 'react-router-dom';
 import Login from './feature/login'
 import Signup from './feature/signup'
-import HomeDashboard from './feature/dashboard'
+import HomeDashboard from './pages/main_page/dashboard'
 import LandingPage from './pages/LandingPage'
-import MemoryLane from './feature/memory-lane'
-import CalorieTracker from './feature/calorie-tracker'
-import BudgetTracker from './feature/budget-tracker'
-import AddMemory from './feature/add-memory';
-import MemoryUploadContainer from './feature/memory-uploader/MemoryUploadContainer';
+import MemoryLane from './pages/main_page/memory-lane'
+import CalorieTracker from './pages/main_page/calorie-tracker'
+import BudgetTracker from './pages/main_page/budget-tracker'
 import MemoryLaneUpload from './feature/memory-uploader/MemoryLaneUpload';
 
 
@@ -18,14 +16,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<Signup />}/>
-        <Route path="/dashboard" element={<HomeDashboard />}/>
-        <Route path="/memory-lane" element={<MemoryLane />}/>
-        <Route path="/add-memory" element={<MemoryLaneUpload />}/>
-        <Route path="/calorie-tracker" element={<CalorieTracker />}/> 
-        <Route path="/budget-tracker" element={<BudgetTracker />}/>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<HomeDashboard />} />
+        <Route path="/memory-lane" element={<MemoryLane />} />
+        <Route path="/add-memory" element={<MemoryLaneUpload />} />
+        <Route path="/calorie-tracker" element={<CalorieTracker />} />
+        <Route path="/budget-tracker" element={<BudgetTracker />} />
       </Routes>
     </>
   )
